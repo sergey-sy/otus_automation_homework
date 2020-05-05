91ae1130ac7698024ff7cd843ce7f433b0a0b7d9
Sergei Sysoev <sergey-sysoev@yandex-team.ru>
2020-05-05 20:15:21 +0300

    move tests from dz4 to dz4/tests

--- dz4/tests/test_check_valid_http_response.py
+++ dz4/tests/test_check_valid_http_response.py
@@ -4,11 +4,6 @@ import requests
 def test_answer(url_param):
     """Compare status code from request with status code from input"""
     response = requests.get(url_param['--url'])
-    if str(response.status_code) == url_param['--status_code']:
-        assert True
-    else:
-        print(
-            f"""\n!response.status_code does not satisfy --status_code from input
+    assert_message = f"""\n!response.status_code does not satisfy --status_code from input
             *** {response.status_code} != {url_param['--status_code']} ***"""
-        )
-        assert False
+    assert str(response.status_code) == url_param['--status_code'], assert_message
